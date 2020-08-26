c668101ea56b8f05cde393189847cfe0e872f18c
むずでょ <muzudho1@gmail.com>
2020-08-27 01:22:36 +0900

    search

--- src/computer_player.rs
+++ src/computer_player.rs
@@ -47,168 +47,10 @@ impl Search {
             // I only look at the empty square.
             // 空きマスだけを見ます。
             if !pos.is_file_fill(*file) {
-                let mut backward_cut_off = None;
-                let mut info_leaf = false;
-                let mut info_backwarding = None;
-                let mut info_result = None;
-                let mut info_comment = None;
-                // Let's put a stone for now.
-                // とりあえず石を置きましょう。
-                pos.do_move(*file);
-                self.nodes += 1;
-
-                // Find out why you are not doing a forward search.
-                // If not, I will search.
-                // 前向き検索を行わない理由を調べてください。
-                // 無ければ探索します。
-                let forward_cut_off = if pos.is_opponent_win() {
-                    // The opponent wins.
-                    // 対戦相手の勝ち。
-                    if Log::enabled(Level::Info) && pos.info_enabled {
-                        info_result = Some(GameResult::Win);
-                        info_comment = Some("Resign.");
-                    }
-                    Some(ForwardCutOff::OpponentWin)
-                } else if SQUARES_NUM <= pos.pieces_num {
-                    // Draw if there is no place to put.
-                    // 置く場所が無ければ引き分け。
-                    if Log::enabled(Level::Info) && pos.info_enabled {
-                        info_leaf = true;
-                        info_result = Some(GameResult::Draw);
-                        info_comment = Some("It is ok.");
-                    }
-                    Some(ForwardCutOff::Draw)
-                } else {
-                    if Log::enabled(Level::Info) && pos.info_enabled {
-                        info_comment = Some("Search.");
-                    }
-                    None
-                };
-
-                // (1) Outputs information for forward search.
-                // (一) 前向き探索の情報を出力します。
-                if pos.info_enabled {
-                    Log::print_info(&Search::info_str(
-                        self.nps(),
-                        self.nodes,
-                        &pos.pv,
-                        SearchDirection::Forward,
-                        *file,
-                        info_leaf,
-                        None,
-                        info_result,
-                        pos.turn,
-                        info_comment,
-                    ));
-                }
-
-                if let None = forward_cut_off {
-                    // If you move forward, it's your opponent's turn.
-                    // 前向きに探索したら、次は対戦相手の番です。
-                    let (_opponent_sq, opponent_game_result) = self.node(pos);
-
-                    // I'm back.
-                    // 戻ってきました。
-                    info_backwarding = Some(opponent_game_result);
-                }
-
-                // (2) Remove the placed stone.
-                // (二) 置いた石は取り除きます。
-                pos.undo_move();
-
-                if let Some(opponent_game_result) = info_backwarding {
-                    match opponent_game_result {
-                        GameResult::Lose => {
-                            // I beat the opponent.
-                            // 相手を負かしました。
-
-                            // The search ends.
-                            // 探索を終了します。
-                            backward_cut_off = Some(BackwardCutOff::YouWin);
-                        }
-                        GameResult::Draw => {
-                            // If neither is wrong, draw.
-                            // お互いがミスしなければ引き分け。
-
-                            match best_result {
-                                GameResult::Lose => {
-                                    // If it gets better, change it to this. Generally called 'Update alpha evaluation'.
-                                    // 良くなるならこの手に変えます。一般的には 'α評価値の更新' と呼びます。
-                                    best_file = Some(*file);
-                                    best_result = GameResult::Draw;
-                                }
-                                _ => {}
-                            }
-                            // I will continue.
-                            // まだ続けます。
-                        }
-                        GameResult::Win => {
-                            // Don't choose to lose.
-                            // 自分が負ける手は選びません。
-
-                            // I will continue.
-                            // まだ続けます。
-                        }
-                    }
-                }
-
-                // (3) Outputs backward search information.
-                // (三) 後ろ向き探索の情報を出力します。
-                if Log::enabled(Level::Info) && pos.info_enabled {
-                    if let Some(opponent_game_result) = info_backwarding {
-                        match opponent_game_result {
-                            GameResult::Lose => {
-                                // I beat the opponent.
-                                // 相手を負かしました。
-                                info_result = Some(GameResult::Win);
-                                info_comment = Some("Hooray!");
-                            }
-                            GameResult::Draw => {
-                                // If neither is wrong, draw.
-                                // お互いがミスしなければ引き分け。
-                                info_result = Some(GameResult::Draw);
-                                info_comment = Some("Fmmm.");
-                            }
-                            GameResult::Win => {
-                                // Don't choose to lose.
-                                // 自分が負ける手は選びません。
-                                info_result = Some(GameResult::Lose);
-                                info_comment = Some("Damn!");
-                            }
-                        }
-                    }
-                    Log::print_info(&Search::info_str(
-                        self.nps(),
-                        self.nodes,
-                        &pos.pv,
-                        SearchDirection::Backward,
-                        *file,
-                        info_leaf,
-                        Some(pos.pieces_num),
-                        info_result,
-                        pos.turn,
-                        info_comment,
-                    ));
-                }
-
-                // (4) Depending on the condition, the sibling node search is skipped.
-                // (四) 条件によっては、兄弟ノードの検索がスキップされます。
-                if let Some(forward_cut_off) = forward_cut_off {
-                    match forward_cut_off {
-                        ForwardCutOff::OpponentWin => {
-                            return (Some(*file), GameResult::Win);
-                        }
-                        ForwardCutOff::Draw => {
-                            return (Some(*file), GameResult::Draw);
-                        }
-                    }
-                } else if let Some(backward_cut_off) = backward_cut_off {
-                    match backward_cut_off {
-                        BackwardCutOff::YouWin => {
-                            return (Some(*file), GameResult::Win);
-                        }
-                    }
-                }
+                let (best_file_child, best_result_child) =
+                    self.node_exit_to_child_side(pos, *file, &mut best_file, &mut best_result);
+                best_file = best_file_child;
+                best_result = best_result_child;
             }
         }
 
@@ -216,6 +58,190 @@ impl Search {
         // 手番の終わり。
         (best_file, best_result)
     }
+
+    fn node_exit_to_child_side(
+        &mut self,
+        pos: &mut Position,
+        file: char,
+        best_file: &mut Option<char>,
+        best_result: &mut GameResult,
+    ) -> (Option<char>, GameResult) {
+        let mut backward_cut_off = None;
+        let mut info_leaf = false;
+        let mut info_backwarding = None;
+        let mut info_result = None;
+        let mut info_comment = None;
+        // Let's put a stone for now.
+        // とりあえず石を置きましょう。
+        pos.do_move(file);
+        self.nodes += 1;
+
+        // Find out why you are not doing a forward search.
+        // If not, I will search.
+        // 前向き検索を行わない理由を調べてください。
+        // 無ければ探索します。
+        let forward_cut_off = if pos.is_opponent_win() {
+            // The opponent wins.
+            // 対戦相手の勝ち。
+            if Log::enabled(Level::Info) && pos.info_enabled {
+                info_result = Some(GameResult::Win);
+                info_comment = Some("Resign.");
+            }
+            Some(ForwardCutOff::OpponentWin)
+        } else if SQUARES_NUM <= pos.pieces_num {
+            // Draw if there is no place to put.
+            // 置く場所が無ければ引き分け。
+            if Log::enabled(Level::Info) && pos.info_enabled {
+                info_leaf = true;
+                info_result = Some(GameResult::Draw);
+                info_comment = Some("It is ok.");
+            }
+            Some(ForwardCutOff::Draw)
+        } else {
+            if Log::enabled(Level::Info) && pos.info_enabled {
+                info_comment = Some("Search.");
+            }
+            None
+        };
+
+        // (1) Outputs information for forward search.
+        // (一) 前向き探索の情報を出力します。
+        if pos.info_enabled {
+            Log::print_info(&Search::info_str(
+                self.nps(),
+                self.nodes,
+                &pos.pv,
+                SearchDirection::Forward,
+                file,
+                info_leaf,
+                None,
+                info_result,
+                pos.turn,
+                info_comment,
+            ));
+        }
+
+        if let None = forward_cut_off {
+            // If you move forward, it's your opponent's turn.
+            // 前向きに探索したら、次は対戦相手の番です。
+            let (_opponent_sq, opponent_game_result) = self.node(pos);
+
+            // I'm back.
+            // 戻ってきました。
+            info_backwarding = Some(opponent_game_result);
+        }
+
+        // (2) Remove the placed stone.
+        // (二) 置いた石は取り除きます。
+        pos.undo_move();
+
+        if let Some(opponent_game_result) = info_backwarding {
+            match opponent_game_result {
+                GameResult::Lose => {
+                    // I beat the opponent.
+                    // 相手を負かしました。
+
+                    // The search ends.
+                    // 探索を終了します。
+                    backward_cut_off = Some(BackwardCutOff::YouWin);
+                }
+                GameResult::Draw => {
+                    // If neither is wrong, draw.
+                    // お互いがミスしなければ引き分け。
+
+                    match best_result {
+                        GameResult::Lose => {
+                            // If it gets better, change it to this. Generally called 'Update alpha evaluation'.
+                            // 良くなるならこの手に変えます。一般的には 'α評価値の更新' と呼びます。
+                            *best_file = Some(file);
+                            *best_result = GameResult::Draw;
+                        }
+                        _ => {}
+                    }
+                    // I will continue.
+                    // まだ続けます。
+                }
+                GameResult::Win => {
+                    // Don't choose to lose.
+                    // 自分が負ける手は選びません。
+
+                    // I will continue.
+                    // まだ続けます。
+                }
+            }
+        }
+
+        // (3) Outputs backward search information.
+        // (三) 後ろ向き探索の情報を出力します。
+        if Log::enabled(Level::Info) && pos.info_enabled {
+            if let Some(opponent_game_result) = info_backwarding {
+                match opponent_game_result {
+                    GameResult::Lose => {
+                        // I beat the opponent.
+                        // 相手を負かしました。
+                        info_result = Some(GameResult::Win);
+                        info_comment = Some("Hooray!");
+                    }
+                    GameResult::Draw => {
+                        // If neither is wrong, draw.
+                        // お互いがミスしなければ引き分け。
+                        info_result = Some(GameResult::Draw);
+                        info_comment = Some("Fmmm.");
+                    }
+                    GameResult::Win => {
+                        // Don't choose to lose.
+                        // 自分が負ける手は選びません。
+                        info_result = Some(GameResult::Lose);
+                        info_comment = Some("Damn!");
+                    }
+                }
+            }
+            Log::print_info(&Search::info_str(
+                self.nps(),
+                self.nodes,
+                &pos.pv,
+                SearchDirection::Backward,
+                file,
+                info_leaf,
+                Some(pos.pieces_num),
+                info_result,
+                pos.turn,
+                info_comment,
+            ));
+        }
+
+        // (4) Depending on the condition, the sibling node search is skipped.
+        // (四) 条件によっては、兄弟ノードの検索がスキップされます。
+        if let Some(forward_cut_off) = forward_cut_off {
+            match forward_cut_off {
+                ForwardCutOff::OpponentWin => {
+                    return (Some(file), GameResult::Win);
+                }
+                ForwardCutOff::Draw => {
+                    return (Some(file), GameResult::Draw);
+                }
+            }
+        } else if let Some(backward_cut_off) = backward_cut_off {
+            match backward_cut_off {
+                BackwardCutOff::YouWin => {
+                    return (Some(file), GameResult::Win);
+                }
+            }
+        }
+
+        return (*best_file, *best_result);
+    }
+
+    /*
+    fn node_enter_from_child_side(
+        &mut self,
+        pos: &mut Position,
+        file: char,
+        best_file: &mut Option<char>,
+        best_result: &mut GameResult,
+    ) -> (Option<char>, GameResult) {
+    }
+    */
 }
 
 /// The reason for ending the forward search.  
