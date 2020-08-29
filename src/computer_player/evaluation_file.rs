b6a281730f49374b02b425b5d73a6a18bd1000c6
むずでょ <muzudho1@gmail.com>
2020-08-29 11:35:28 +0900

    save

--- src/computer_player/evaluation_file.rs
+++ src/computer_player/evaluation_file.rs
@@ -5,19 +5,51 @@ use std::path::Path;
 
 impl Evaluation {
     /// Save to a file.  
-    /// TODO ファイルへ保存します。  
+    /// ファイルへ保存します。  
     pub fn save(&self, file_name: &str) {
         let mut text = String::new();
         // Open the file.
         let file = OpenOptions::new()
-            .create(true)
-            .append(true)
+            .write(true)
+            .truncate(true)
             .open(Path::new(file_name))
             // TODO error handling.
             .unwrap();
 
-        for feat in &self.features_1_to_7 {
-            for win_draw in feat.iter() {
+        for state in &self.features_1_to_7 {
+            for win_draw in state.iter() {
+                for value in win_draw.iter() {
+                    text.push_str(&format!("{},", value));
+                }
+            }
+            text.push_str("\r\n");
+        }
+        for state in &self.features_8_to_13 {
+            for win_draw in state.iter() {
+                for value in win_draw.iter() {
+                    text.push_str(&format!("{},", value));
+                }
+            }
+            text.push_str("\r\n");
+        }
+        for state in &self.features_14_19_20_25 {
+            for win_draw in state.iter() {
+                for value in win_draw.iter() {
+                    text.push_str(&format!("{},", value));
+                }
+            }
+            text.push_str("\r\n");
+        }
+        for state in &self.features_15_18_21_24 {
+            for win_draw in state.iter() {
+                for value in win_draw.iter() {
+                    text.push_str(&format!("{},", value));
+                }
+            }
+            text.push_str("\r\n");
+        }
+        for state in &self.features_16_17_22_23 {
+            for win_draw in state.iter() {
                 for value in win_draw.iter() {
                     text.push_str(&format!("{},", value));
                 }
@@ -34,6 +66,6 @@ impl Evaluation {
     }
 
     /// Load from a file.  
-    /// TODO ファイルへ保存します。  
+    /// TODO ファイルから読み込みます。  
     pub fn load(&self) {}
 }
